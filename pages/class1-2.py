de037d7595220d9d1ef669cb43f04a21b239e1b9
NaiSyuan <jeffrey1234567891047@gmail.com>
2024-08-12 16:43:34 +0800

    1130812

--- pages/class1-2.py
+++ pages/class1-2.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
 st.title("")
+st.markdown(
+    """
+    # 這是大標題
+
+-
+-
+-
+
+## 這是第二大標題
+
+---
+```python 
+print("Hello world")
+```
+### 這是第三大標題
+
+#### 這是第四大標題
+
+##### 這是第五大標題
+
+###### 這是第六大標題
+
+"""
+)
